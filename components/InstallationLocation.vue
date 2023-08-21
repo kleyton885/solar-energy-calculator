233b9ccb1f32a32c3c94b490db7a92b30757a837
kleyton885 <kleyton_afonso@hotmail.com>
2023-08-21 18:52:14 -0300

    added map to home

---
+++ components/InstallationLocation.vue
@@ -0,0 +1,43 @@
+<template>
+  <client-only>
+    <VRow no-gutters align="center" justify="center" class="mt-4">
+      <VCol class="fill-height" md="8" lg="8">
+        <div>
+          <h3>1. Aponte o local onde pretende realizar a instalação</h3>
+        </div>
+        <div id="map-wrap" style="height: 60vh;">
+          <l-map @click="setLocation" :zoom=13 :center="[-9.643112, -35.718860]">
+            <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+            <l-marker :lat-lng=latlng>
+              <l-popup>{{ address }}</l-popup>
+            </l-marker>
+          </l-map>
+        </div>
+      </VCol>
+      <VCol class="fill-height" md="8" lg="8">
+        {{ address }}
+      </VCol>
+    </VRow>
+  </client-only>
+</template>
+
+<script setup>
+const latlng = ref({lat: '', lng: ''});
+const address = ref('');
+
+async function setLocation(e) {
+  latlng.value = e.latlng
+  try {
+    const post = await fetch(`http://nominatim.openstreetmap.org/reverse?lat=` + latlng.value.lat
+      + `&lon=` + e.latlng.lng
+      + `&format=json`
+      + `&addressdetails=1`
+      + `&accept-language=pt-BR`
+      + `&zoom=18`).then((r) => r.json())
+    address.value = post.address.road ? post.address.road + ', ' + post.address.suburb + ', ' + post.address.city + ', ' + post.address.state + ', ' + post.address.postcode : ''
+  } catch (e) {
+    console.log(e)
+  }
+
+}
+</script>
---
+++ components/InstallationLocationType.vue
@@ -0,0 +1,23 @@
+<template>
+    <VRow no-gutters align="center" justify="center">
+        <VForm @submit.prevent="submit" class="mt-7">
+            <div>
+                <label class="label text-grey-darken-2" for="name">1. Qual é a categoria do ambiente onde o gerador será
+                    instalado?</label>
+                <VTextField :rules="[ruleRequired]" v-model="name" prepend-inner-icon="fluent:person-24-regular" id="name"
+                    name="name" />
+            </div>
+            <div class="mt-5">
+                <VBtn type="submit" block min-height="45" class="gradient primary">Create Account</VBtn>
+            </div>
+        </VForm>
+    </VRow>
+</template>
+
+<script setup>
+const name = ref("");
+const email = ref("");
+const password = ref("");
+
+const submit = async () => { };
+</script>
