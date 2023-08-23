ebda4cfa12651f478ca2741f96cab58be31e7933
kleyton885 <kleyton_afonso@hotmail.com>
2023-08-23 19:22:58 -0300

    capacity is now monthly

--- components/ProductionCapacity.vue
+++ components/ProductionCapacity.vue
@@ -1,7 +1,7 @@
 <template>
   <VRow v-if="getSolarInfo()" no-gutters align="center" justify="center" class="mt-4">
     <VCol v-if="capacity" class="fill-height" cols="12" md="8" lg="8">
-      <v-card width="200">
+      <v-card width="230">
         <v-img
           height="75"
           src="img/energia-solar.png"
@@ -11,15 +11,15 @@
 
         <v-card-text>
           <div class="font-weight-bold ms-1 mb-2 d-flex flex-column align-items-center">
-            <div class="text-center">Produção Anual*</div>
-            <div class="text-center text-h6 font-weight-black text-green-darken-1">{{ capacity }} kWh/ ano</div>
+            <div class="text-center">Produção Mensal Estimada*</div>
+            <div class="text-center text-h6 font-weight-black text-green-darken-1">{{ capacity }} kWh/ mês</div>
           </div>
         </v-card-text>
       </v-card>
-      <div class="mt-2">* Considerando um sistema de 3.3 kWp (6 módulos de 555W)</div>
+      <div class="mt-2">* Considerando a potência de um kit de 3.3 kWp (6 módulos de 555W)</div>
     </VCol>
     <VCol cols="12" md="8" lg="8" v-else>
-      <v-skeleton-loader v-if="latlng.lat != ''" width="200" height="75" type="image" />
+      <v-skeleton-loader v-if="latlng.lat != ''" width="230" height="75" type="image" />
     </VCol>
   </VRow>
 </template>
@@ -35,14 +35,14 @@ async function getSolarInfo(e) {
     if (latlng.value.lat != '') {
       const post = await $fetch('https://api.globalsolaratlas.info/data/pvcalc?loc='+latlng.value.lat+','+latlng.value.lng, {
         method: 'POST',
-        body: '{"type":"rooftopSmall","systemSize":{"type":"capacity","value":3.3},"orientation":{"azimuth":0,"tilt":9}}'
+        body: '{"type":"rooftopSmall","systemSize":{"type":"capacity","value":3.33},"orientation":{"azimuth":0,"tilt":9}}'
       });
       capacity.value = post.annual.data.PVOUT_total
 
       if (capacity.value % 1 >= 0.5) {
-        capacity.value = Math.ceil(capacity.value);
+        capacity.value = Math.ceil(capacity.value/12);
       } else {
-        capacity.value = Math.floor(capacity.value);
+        capacity.value = Math.floor(capacity.value/12);
       }
 
 
